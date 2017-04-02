152c0927e7739dcd14b4180ba0679541219af0a6
Rama Milaneh <ramamilaneh@gmail.com>
2017-04-02 15:04:17 -0400

    added some changes

--- Video-Background/ViewController.m
+++ Video-Background/ViewController.m
@@ -16,7 +16,6 @@
 @property (nonatomic, strong) UILabel *alreadyUser;
 @property (nonatomic, strong) UILabel *welcomeLabel;
 
-
 @end
 
 @implementation ViewController
@@ -40,6 +39,9 @@
 }
 
 -(void)setupViews {
+    
+    UIColor *buttonColor = [UIColor colorWithRed:91/255.0 green:155/255.0 blue:171/255.0 alpha:1];
+    // Log in button setup
     _logInButton = [UIButton buttonWithType: UIButtonTypeSystem];
     [self.view addSubview:_logInButton];
     _logInButton.translatesAutoresizingMaskIntoConstraints = NO;
@@ -47,16 +49,17 @@
     [[_logInButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor] setActive:YES];
     [[_logInButton.widthAnchor constraintEqualToConstant:0.7*self.view.frame.size.width] setActive:YES];
     [[_logInButton.heightAnchor constraintEqualToConstant:40] setActive:YES];
+    
     [_logInButton setTitle:@"LOG IN" forState:UIControlStateNormal];
     _logInButton.titleLabel.font = [UIFont systemFontOfSize:16];
     _logInButton.clipsToBounds = YES;
     [_logInButton.layer setBorderWidth:3.0];
     [_logInButton.layer setBorderColor:[UIColor whiteColor].CGColor];
     _logInButton.layer.cornerRadius = 20;
-    UIColor *buttonColor = [UIColor colorWithRed:91/255.0 green:155/255.0 blue:171/255.0 alpha:1];
     [_logInButton setBackgroundColor:[UIColor clearColor]];
     [_logInButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
 
+    // Create account button setup
     _createAnAccountButton = [UIButton buttonWithType: UIButtonTypeSystem];
     [self.view addSubview:_createAnAccountButton];
     _createAnAccountButton.translatesAutoresizingMaskIntoConstraints = NO;
@@ -64,6 +67,7 @@
     [[_createAnAccountButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor] setActive:YES];
     [[_createAnAccountButton.widthAnchor constraintEqualToConstant:0.7*self.view.frame.size.width] setActive:YES];
     [[_createAnAccountButton.heightAnchor constraintEqualToConstant:40] setActive:YES];
+    
     [_createAnAccountButton setTitle:@"CREATE ACCOUNT" forState:UIControlStateNormal];
     _createAnAccountButton.titleLabel.font = [UIFont systemFontOfSize:16];
     _createAnAccountButton.clipsToBounds = YES;
@@ -71,27 +75,23 @@
     [_createAnAccountButton setBackgroundColor:buttonColor];
     [_createAnAccountButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     
+    // already user label setup
     _alreadyUser = [[UILabel alloc] init];
-    [_alreadyUser setTextColor:[UIColor blackColor]];
-    [_alreadyUser setText:NSLocalizedString(@"Already a user?", nil)];
-    [_alreadyUser setAdjustsFontSizeToFitWidth:YES];
-    [_alreadyUser setFont:[UIFont systemFontOfSize:12]];
-    [_alreadyUser setTextColor:[UIColor whiteColor]];
-    
     [self.view addSubview:_alreadyUser];
     _alreadyUser.translatesAutoresizingMaskIntoConstraints = NO;
     [[_alreadyUser.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor] setActive:YES];
     [[_alreadyUser.bottomAnchor constraintEqualToAnchor:self.logInButton.topAnchor constant:-10] setActive:YES];
     [[_alreadyUser.heightAnchor constraintEqualToConstant:22] setActive:YES];
     [[_alreadyUser.widthAnchor constraintEqualToConstant:100] setActive:YES];
-
-    _welcomeLabel= [[UILabel alloc] init];
-    [_welcomeLabel setTextColor:[UIColor blackColor]];
-    [_welcomeLabel setText:NSLocalizedString(@"  WELCOME", nil)];
-    [_welcomeLabel setAdjustsFontSizeToFitWidth:YES];
-    [_welcomeLabel setFont:[UIFont boldSystemFontOfSize:36]];
-    [_welcomeLabel setTextColor:[UIColor whiteColor]];
     
+    [_alreadyUser setTextColor:[UIColor blackColor]];
+    [_alreadyUser setText:NSLocalizedString(@"Already a user?", nil)];
+    [_alreadyUser setAdjustsFontSizeToFitWidth:YES];
+    [_alreadyUser setFont:[UIFont systemFontOfSize:12]];
+    [_alreadyUser setTextColor:[UIColor whiteColor]];
+
+    // welcome label setup
+    _welcomeLabel= [[UILabel alloc] init];
     [self.view addSubview:_welcomeLabel];
     _welcomeLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [[_welcomeLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor] setActive:YES];
@@ -99,7 +99,11 @@
     [[_welcomeLabel.heightAnchor constraintEqualToConstant:50] setActive:YES];
     [[_welcomeLabel.widthAnchor constraintEqualToConstant:200] setActive:YES];
     
-
+    [_welcomeLabel setTextColor:[UIColor blackColor]];
+    [_welcomeLabel setText:NSLocalizedString(@"  WELCOME", nil)];
+    [_welcomeLabel setAdjustsFontSizeToFitWidth:YES];
+    [_welcomeLabel setFont:[UIFont boldSystemFontOfSize:36]];
+    [_welcomeLabel setTextColor:[UIColor whiteColor]];
     
 }
 - (void)didReceiveMemoryWarning {
